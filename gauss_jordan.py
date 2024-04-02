c9b197db872bf5e854bf180cba5e7f62569a2223
Jeison Angel <jestean347@gmail.com>
2024-04-01 20:43:45 -0500

    Modificación Gauss Jordan

--- gauss_jordan.py
+++ gauss_jordan.py
@@ -5,9 +5,10 @@ number_columns = int(input('Escriba el número de incógnitas de las ecuaciones:
 number_equation = number_columns
 number_columns +=1
 array = np.zeros((number_equation, number_columns))
+_RES_COLUMN = "res"
 print(array)
 matriz_df = pd.DataFrame(array,columns=[f"x{i+1}" for i in range(number_columns)],index=[f"ec{i+1}" for i in range(number_equation)])
-matriz_df.columns = matriz_df.columns[:-1].tolist() + ["res"]
+matriz_df.columns = matriz_df.columns[:-1].tolist() + [_RES_COLUMN]
 
 print('range(number_equation)')
 print(range(number_equation))
@@ -18,7 +19,9 @@ for row_index in range(number_equation):
 
 def transform_pivot_to_1(matriz, pivot):
     print(f'F{pivot} => F{pivot} / {matriz.iloc[pivot,pivot]} ---------------')
-    if (matriz.iloc[pivot,pivot] == 0): return (matriz,False)
+    if (matriz.iloc[pivot,pivot] == 0): 
+        # matriz = prepare_matrix(matriz)
+        return (matriz,True)
     matriz.iloc[pivot] = matriz.iloc[pivot] / matriz.iloc[pivot,pivot]
     return (matriz,True)
 
@@ -40,19 +43,44 @@ def gauss_jordan(matriz, pivot):
     for row_count in range(matriz.shape[0]):
         print(f'row_count => {row_count}')
         if not pivote_transform:
-            (matriz,isValid) = transform_pivot_to_1(matriz,pivot)
-            if not isValid:
-                return (matriz,False)
-            print(matriz)
+            if (matriz.iloc[pivot,row_count] != 1): 
+                (matriz,isValid) = transform_pivot_to_1(matriz,pivot)
+                if not isValid:
+                    return (matriz,False)
+                print(matriz)
             pivote_transform = True
-        (matriz,isValid) = transform_to_0(matriz,pivot,row_count)
+        if (matriz.iloc[pivot,row_count] != 0): 
+            (matriz,isValid) = transform_to_0(matriz,pivot,row_count)
         print(matriz)
     return (matriz,True)
 
+def prepare_matrix(matriz, pivot):
+    row = matriz.iloc[pivot]
+    for i in range(matriz.shape[0]):
+        if (matriz.iloc[i,pivot] != 0):
+            row2 = matriz.iloc[i]
+            matriz.iloc[i]
+            
+def validate_matrix(matriz):
+    for i in matriz.columns:
+        if i != _RES_COLUMN:
+            ceros = matriz[matriz[i] == 0]
+            if ceros.shape[0] == matriz.shape[0]:
+                print('LA MATRIZ NO TIENE SOLUCIÓN')
+                return matriz, True
+                break
+    return matriz, True
+
 print(matriz_df)
-for count_column in range(matriz_df.columns.size-1):
-    (matriz_df,isValid) = gauss_jordan(matriz_df, count_column)
-    if not isValid:
-        print('La matriz no tiene solución')
-        break
-# print(matriz_df)
+print('VALIDATE')
+isValid = False
+matriz_df,isValid = validate_matrix(matriz_df)
+
+if isValid:
+    for count_column in range(matriz_df.columns.size-1):
+        matriz_df = prepare_matrix(matriz_df)
+        (matriz_df,isValid) = gauss_jordan(matriz_df, count_column)
+        if not isValid:
+            print('La matriz no tiene solución')
+            break
+    print(matriz_df)
